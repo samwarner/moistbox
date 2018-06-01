debb200fc7109fe11429f5e8eb85949bf95dfbab
Samuel Warner <samwarner.uk@gmail.com>
2018-06-01 13:31:05 +0100

    Added to Main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,31 @@
 import time
+import bme680
+
 
 def main():
-    print("Thanks.")
+    sensor = bme680.BME680()
+    # These oversampling settings can be tweaked to
+    # change the balance between accuracy and noise in
+    # the data.
+
+    sensor.set_humidity_oversample(bme680.OS_2X)
+    sensor.set_pressure_oversample(bme680.OS_4X)
+    sensor.set_temperature_oversample(bme680.OS_8X)
+    sensor.set_filter(bme680.FILTER_SIZE_3)
+
+    print("Poling:")
+    try:
+        while True:
+            if sensor.get_sensor_data():
+                output = f"""Temp.: 
+                    {sensor.data.temperature} C, 
+                    Hum.: {sensor.data.pressure} HU, 
+                    Pres.: {sensor.data.humidity} hPa."""
+
+            print(output)
+
+    except KeyboardInterupt:
+        pass
 
 
 if __name__ == "__main__":
